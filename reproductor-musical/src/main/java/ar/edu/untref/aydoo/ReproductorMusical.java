6d9f517af49e52ae3a101f556a0587f3bca61afb
Lucas <lucasdcampaner@gmail.com>
2018-03-28 20:59:28 -0300

    Agrega canción a la lista de reproducción.

--- reproductor-musical/src/main/java/ar/edu/untref/aydoo/ReproductorMusical.java
+++ reproductor-musical/src/main/java/ar/edu/untref/aydoo/ReproductorMusical.java
@@ -1,4 +1,14 @@
 package ar.edu.untref.aydoo;
 
 public class ReproductorMusical {
+
+    public void agregarCancion(Cancion cancion) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    public boolean tieneLaCancion(Cancion cancion) {
+        // TODO Auto-generated method stub
+        return true;
+    }
 }
--- reproductor-musical/src/test/java/ar/edu/untref/aydoo/ReproductorMusicalTest.java
+++ reproductor-musical/src/test/java/ar/edu/untref/aydoo/ReproductorMusicalTest.java
@@ -1,5 +1,22 @@
 package ar.edu.untref.aydoo;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class ReproductorMusicalTest {
     
+    
+    @Test
+    public void agregarCancionLaAgregaAListaDeReproduccion() {
+        
+        ReproductorMusical reproductorMusical = new ReproductorMusical();
+        Cancion cancion = new Cancion();
+        
+        reproductorMusical.agregarCancion(cancion);
+        
+        Assert.assertTrue(reproductorMusical.tieneLaCancion(cancion));
+        
+    }
+
+
 }
