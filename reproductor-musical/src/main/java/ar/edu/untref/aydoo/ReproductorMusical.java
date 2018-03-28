ca39b0326de5b2011b092e4f5f2aaf0183b968e1
Lucas <lucasdcampaner@gmail.com>
2018-03-28 19:29:14 -0300

    Borra la resolución para hacer la demo.

--- reproductor-musical/src/main/java/ar/edu/untref/aydoo/ReproductorMusical.java
+++ reproductor-musical/src/main/java/ar/edu/untref/aydoo/ReproductorMusical.java
@@ -1,40 +1,4 @@
 package ar.edu.untref.aydoo;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class ReproductorMusical {
-
-    private List<Cancion> listaDeCanciones;
-
-    public ReproductorMusical() {
-        listaDeCanciones = new ArrayList<>();
-    }
-
-    public void agregarCancion(Cancion cancion) {
-        listaDeCanciones.add(cancion);
-    }
-
-    public List<Cancion> obtenerListaDeCanciones() {
-        List<Cancion> canciones = new ArrayList<>(listaDeCanciones);
-        return canciones;
-    }
-
-    public void agregarListaDeCanciones(List<Cancion> listaDeCanciones) {
-        this.listaDeCanciones.addAll(listaDeCanciones);
-    }
-
-    public int obtenerDuracionTotalDeReproduccion() {
-
-        int duracionTotal = 0;
-        for (Cancion cancion: listaDeCanciones) {
-            duracionTotal += cancion.getDuracion();
-        }
-        return duracionTotal;
-    }
-
-    public boolean tieneLaCancion(Cancion cancion) {
-        return this.listaDeCanciones.contains(cancion);
-    }
-
 }
