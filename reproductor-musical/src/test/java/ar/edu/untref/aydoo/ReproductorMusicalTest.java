7b151a3f3755d18466d187a43eab8700a18ed9b0
Lucas <lucasdcampaner@gmail.com>
2018-03-28 21:27:24 -0300

    Agrega metodo de inicializacion.

--- reproductor-musical/src/test/java/ar/edu/untref/aydoo/ReproductorMusicalTest.java
+++ reproductor-musical/src/test/java/ar/edu/untref/aydoo/ReproductorMusicalTest.java
@@ -1,31 +1,36 @@
 package ar.edu.untref.aydoo;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class ReproductorMusicalTest {
-    
-    
+
+    private ReproductorMusical reproductorMusical;
+
+    @Before
+    public void inicializacion() {
+        reproductorMusical = new ReproductorMusical();
+    }
+
     @Test
     public void agregarCancionLaAgregaAListaDeReproduccion() {
-        
-        ReproductorMusical reproductorMusical = new ReproductorMusical();
+
         Cancion cancion = new Cancion();
-        
+
         reproductorMusical.agregarCancion(cancion);
-        
+
         Assert.assertTrue(reproductorMusical.tieneLaCancion(cancion));
-        
+
     }
-    
+
     @Test
     public void noAgregoUnaCancionEntoncesNoExisteEnMiReproductor() {
-        
-        ReproductorMusical reproductorMusical = new ReproductorMusical();
+
         Cancion miPerroDinamita = new Cancion();
-        
+
         Assert.assertFalse(reproductorMusical.tieneLaCancion(miPerroDinamita));
-        
+
     }
 
 }
