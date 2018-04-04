b79e381aca0fc7fd04426b92742ee7eb50b40241
Lucas <lucasdcampaner@gmail.com>
2018-04-04 20:08:37 -0300

    Crea el proyecto base para la batalla naval.

---
+++ batalla-naval/src/main/java/ar/edu/untref/aydoo/Program.java
@@ -0,0 +1,7 @@
+package ar.edu.untref.aydoo;
+
+public class Program {
+    public static final void main(String arg[]) {
+
+    }
+}
---
+++ batalla-naval/src/test/java/ar/edu/untref/aydoo/IntegracionTest.java
@@ -0,0 +1,21 @@
+package ar.edu.untref.aydoo;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class IntegracionTest {
+	
+	@Test
+	public void test() {
+		
+		
+		//A
+		
+		//A
+		
+		Assert.assertTrue(true);
+		
+	}
+
+	
+}
