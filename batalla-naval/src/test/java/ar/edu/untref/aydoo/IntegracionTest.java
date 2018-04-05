097393e216de4e1a3a556472d12cda69373caf0f
Lucas <lucasdcampaner@gmail.com>
2018-04-04 21:44:17 -0300

    Implementa poner bote.

--- batalla-naval/src/test/java/ar/edu/untref/aydoo/IntegracionTest.java
+++ batalla-naval/src/test/java/ar/edu/untref/aydoo/IntegracionTest.java
@@ -4,20 +4,28 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class IntegracionTest {
-	
-	@Test
-	public void ponerBoteEnTablero() {
-		
-		Bote unBote = new Bote();
-		Tablero unTablero = new Tablero();
-		Posicion posicion = new Posicion(1, 1);
-		
-		unTablero.ponerBote(unBote, posicion);
-		
-		Assert.assertFalse(unTablero.estaDisponible(posicion));
-		
-	}
-	
 
-	
+    @Test
+    public void ponerBoteEnTablero() {
+
+        Bote unBote = new Bote();
+        Tablero unTablero = new Tablero();
+        Posicion posicion = new Posicion(1, 1);
+
+        unTablero.ponerBote(unBote, posicion);
+
+        Assert.assertFalse(unTablero.estaDisponible(posicion));
+
+    }
+
+    @Test
+    public void tableroVacioTodasLasPosicionesLibres() {
+
+        Tablero unTablero = new Tablero();
+        Posicion posicion = new Posicion(1, 1);
+
+        Assert.assertTrue(unTablero.estaDisponible(posicion));
+
+    }
+
 }
